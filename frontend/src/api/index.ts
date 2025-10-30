74a49ce407fd4c8784fca9632c14bd1d1f99d848
shadichy <shadichy@blisslabs.org>
2025-10-30 19:21:27 +0700

    feat(frontend): get api endpoint from env var

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import type { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_ENDPOINT || 'http://localhost:8000',
 });
 
 export default api;
